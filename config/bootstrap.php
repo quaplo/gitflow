9eceb36730f53e598a3715e76080785b5363c2e0
Gregor Demo <demo.gregor@gmail.com>
2016-05-26 19:31:44 +0200

    DebugKit pugin load

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,4 +1,20 @@
 <?php
+
+Plugin::load('DebugKit');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
